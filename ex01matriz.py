6d9685cf7b054d67e08b32a0b145ba3b1849a9fd
arthurpansera <163167633+arthurpansera@users.noreply.github.com>
2024-05-22 10:49:41 -0300

    Exercícios dos slides
    Exercícios para praticar

---
+++ ex01matriz.py
@@ -0,0 +1,24 @@
+#Crie uma matriz de tamanho 3x3 e preencha-a com valores de 1 a 9.
+#Imprima a matriz das três formas:
+#1. print(matriz)
+#2. utilizando um for
+#3. utilizando dois for's
+
+matriz = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
+print("\n")
+
+print(matriz)
+print("\n")
+
+for linha in range(3):
+    print(matriz[linha])
+print("\n")
+
+for linha in range(3):
+    for coluna in range(3):
+        print(matriz[linha][coluna])
+print("\n")
